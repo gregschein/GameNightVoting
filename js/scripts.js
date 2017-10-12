894ff1e089fb5c2457c511170062eb454cba6a89
Greg Schein <gregschein@gmail.com>
2017-10-11 17:27:33 -0700

    Initial Commit. Just making functionality.

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+var games = [
+    'Agricola',
+    'Spartacus',
+    'Keyflower',
+    "Five Tribes",
+];
+
+$(document).ready(function(){
+    createRows(games);
+    bindForm();
+    bindSubmit();    
+});
+
+function bindForm() {
+    $('input[type=radio]').click(function() {
+        verticalExclusive($(this));
+    });
+
+    $('input[type=radio]').click(function() {
+        verticalExclusive($(this));
+    });
+
+    $('input[type=radio]').click(function() {
+        verticalExclusive($(this));
+    });
+
+    $('input[type=radio]').click(function() {
+        verticalExclusive($(this));
+    });
+}
+
+function bindSubmit() {
+    $('button[type=button]').click(function() {
+        submitForm();
+    });
+}
+
+function verticalExclusive(element) {
+    var col = element.data('col');
+    $('input[data-col=' + col + ']').prop('checked', false);
+    element.prop('checked', true);
+}
+
+function createRows(games) {
+    for (i=0; i<games.length; i++) {
+        var template = `
+            <tr>
+                <td>`+ games[i] + `</td> 
+                <td><input type="radio" name=` + games[i].replace(/ /g,'') + ` data-col="1"/></td>
+                <td><input type="radio" name=` + games[i].replace(/ /g,'') + ` data-col="2"/></td>
+            </tr>`;
+        $('#gametable').append(template);
+    }
+}
+
+function submitForm() {
+    var firstChoice = $('input[data-col=1]:checked').attr('name');
+    var secondChoice = $('input[data-col=2]:checked').attr('name');
+    console.log(firstChoice + ' ' + secondChoice);
+}
