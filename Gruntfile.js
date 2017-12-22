c40abab4e98a083ddd430783c8b4c5e44ea284f8
Greg Schein <gregschein@gmail.com>
2017-12-21 16:21:51 -0800

    Working on improving the webpage look.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      jasmine : {
+      jasmine: {
         // Your project's source files
-        src : 'js/**/*.js',
+        src: 'js/**/*.js',
         // Your Jasmine spec files
         options: {
-          specs : 'spec/**/*Spec.js',
-          helpers : 'spec/helpers/*.js'
-        }
-        
+          specs: 'spec/**/*Spec.js',
+          helpers: 'spec/helpers/*.js',
+        },
         // Your spec helper files
-        
-      }
+      },
     });
-  
     // Register tasks.
     grunt.loadNpmTasks('grunt-contrib-jasmine');
-  
     // Default task.
     grunt.registerTask('default', 'jasmine');
 };
--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,57 @@
-var games = [
-    'Agricola',
+'use strict';
+
+let games = [
     'Spartacus',
     'Keyflower',
-    "Five Tribes",
+    'Agricola',
+    'Five Tribes',
 ];
 $(document).ready(function() {
     createRows(games);
     bindForm();
     bindSubmit();
 });
-
+/** Binds verticalExlusive to radio buttons. */
 function bindForm() {
     $('input[type=radio]').click(function() {
         verticalExclusive($(this));
     });
 };
-
+/** Binds Submit button*/
 function bindSubmit() {
     $('button[type=button]').click(function() {
         submitForm();
     });
 }
-
+/** Prevents multiple votes for same rank.
+ * @param {element} element - stuff.
+ */
 function verticalExclusive(element) {
-    var col = element.data('col');
+    let col = element.data('col');
     $('input[data-col=' + col + ']').prop('checked', false);
     element.prop('checked', true);
 }
-
+/** Creates table rows to allow voting on games.
+ * @param {list} games - List of Games that can be voted on.
+ */
 function createRows(games) {
-    for (i=0; i<games.length; i++) {
-        var template = `
+    for (let i=0; i<games.length; i++) {
+        let template = `
             <tr>
                 <td>`+ games[i] + `</td> 
-                <td><input type="radio" name=` + games[i].replace(/ /g,'') + ` data-col="1"/></td>
-                <td><input type="radio" name=` + games[i].replace(/ /g,'') + ` data-col="2"/></td>
+                <td><input type="radio" name=`
+                 + games[i].replace(/ /g, '') + ` data-col="1"/></td>
+                <td><input type="radio" name=`
+                 + games[i].replace(/ /g, '') + ` data-col="2"/></td>
             </tr>`;
         $('#gametable').append(template);
     }
 }
-
+/** Submits choices for first and second.
+ */
 function submitForm() {
-    var firstChoice = $('input[data-col=1]:checked').attr('name');
-    var secondChoice = $('input[data-col=2]:checked').attr('name');
+    let firstChoice = $('input[data-col=1]:checked').attr('name');
+    let secondChoice = $('input[data-col=2]:checked').attr('name');
     console.log(firstChoice + ' ' + secondChoice);
 }
 
