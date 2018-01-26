bd52948114928bb28bf301956b725eb9b9bf0a9a
Greg Schein <gregschein@gmail.com>
2018-01-26 14:26:17 -0800

    Working on getting firebase integrated

--- app/player-login/player-login.component.js
+++ app/player-login/player-login.component.js
@@ -5,6 +5,21 @@ angular.
     component('playerLogin', {
         templateUrl: 'player-login/player-login.template.html',
         controller: function PlayerLoginController() {
-            this.test = 'did it work';
+            let self = this;
+            let firebaseObj = new Firebase('https://gamenightvoting.firebaseio.com/');
+            let loginObj = new FirebaseSimpleLogin(firebaseObj);
+            self.signIn = function() {
+                let username = self.user.email;
+                let password = self.user.password;
+                loginObj.$login('password', {
+                    email: username,
+                    password: password,
+                })
+                .then(function(user) {
+                    alert('we gud');
+                }, function(error) {
+                    alert('shit fukt');
+                });
+            };
         },
 });
